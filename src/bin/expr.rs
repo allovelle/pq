6a720c373f38d9d63b49b72a347dfcf5ef4cf1f3
Allovelle <167846019+allovelle@users.noreply.github.com>
2025-11-14 18:51:51 -0500

    expr and starlark

---
+++ src/bin/expr.rs
@@ -0,0 +1,32 @@
+use expr::{self, Context, Environment};
+
+fn main()
+{
+    let mut ctx = Context::default();
+    ctx.insert("two".to_string(), 2);
+
+    let three: i64 = expr::eval("1 + two", &ctx).unwrap().as_number().unwrap();
+    assert_eq!(three, 3);
+
+    let mut env = Environment::new();
+    env.add_function("add", |c| {
+        let mut sum = 0;
+        for arg in c.args
+        {
+            sum += arg.as_number().unwrap();
+        }
+        Ok(sum.into())
+    });
+
+    let six: i64 =
+        env.eval("add(1, two, 3)", &ctx).unwrap().as_number().unwrap();
+
+    ctx.insert("name".to_string(), "Alo");
+
+    println!(
+        "expr `name matches '^[Aa]lo'` = {}",
+        expr::eval("name matches '^[Aa]lo'", &ctx).unwrap()
+    );
+
+    assert_eq!(six, 6);
+}
