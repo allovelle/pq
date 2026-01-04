3c2a6dc1da0379213526c12107dd7e4192a48eb9
Allovelle <167846019+allovelle@users.noreply.github.com>
2026-01-03 22:34:00 -0500

    Started parser code

---
+++ src/bin/parse.rs
@@ -0,0 +1,13 @@
+use pq::ast::ParseResult;
+use pq::tok::Tok;
+
+fn main() -> ParseResult<()>
+{
+    /*  pq::ast::build_ast_table(&[
+        Tok::ArrayOpen,
+        Tok::Number(818.0),
+        Tok::ArrayClose,
+    ])?; */
+
+    Ok(())
+}
