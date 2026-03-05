310f239c4541c6aa8f23195ccf75365523ca83e2
Allovelle <167846019+allovelle@users.noreply.github.com>
2026-03-05 15:47:09 -0500

    PROMPT inc

---
+++ samples/attr.rs
@@ -0,0 +1,54 @@
+//! Row attributes to make editing, querying, and formatting easier.
+
+use crate::parser::Row;
+use smallvec::SmallVec;
+use strum::VariantNames;
+
+// These are examples, there is clearly a pattern that all rows follow
+#[derive(VariantNames)]
+#[repr(i8)]
+pub enum Attr
+{
+    NotEmpty = -1, // ! Could be more expressive like this
+    Empty = 1,
+    EndArr = -2,
+    Array = 2,
+    Last,
+    Lone,
+    EndArray,
+    EndObject,
+    EndParent,
+    Sibling,
+    Key,
+    Parent,
+    Object,
+    Value,
+}
+
+// end par: (query par to get arr or obj ty)
+// where is last *exactly* calculated?
+// maybe: sibling = comma
+// maybe: key = colon
+// maybe: last = no comma
+// maybe: lone = very low cost to wrap (dynamic programming?)
+// quora: when is this *not* a sibling? lone?
+
+pub fn attributes(row: Row, _table: &Vec<Row>) -> SmallVec<[Attr; 8]>
+{
+    // smallvec![Attr::Sibling, Attr::Value];
+    let mut attrs: SmallVec<[Attr; 8]> = SmallVec::new();
+    attrs.push(Attr::Key);
+    attrs.push(Attr::Sibling);
+
+    match row.ty
+    {
+        crate::parser::RowType::Obj => attrs.push(Attr::Object),
+        crate::parser::RowType::Arr => attrs.push(Attr::Array),
+        crate::parser::RowType::Str => attrs.push(Attr::Value),
+        crate::parser::RowType::Num => attrs.push(Attr::Value),
+        crate::parser::RowType::Bit => attrs.push(Attr::Value),
+        crate::parser::RowType::Nil => attrs.push(Attr::Value),
+    }
+
+    attrs
+}
