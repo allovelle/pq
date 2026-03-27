90cc30acd66ae4a7ac668a12d188ec1a65bd234d
Allovelle <167846019+allovelle@users.noreply.github.com>
2026-03-27 13:10:28 -0400

    data types

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
+/*
+1. The types keep pointing back to the previous stages.
+
+2. The stage types like Parser must be all public so helper functions can have a
+    flexible API.
+*/
+
 fn main() {}
